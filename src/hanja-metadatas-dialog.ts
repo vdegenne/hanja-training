9f6b82e44650191e63d5397058488eefa28ce03f
valentin degenne <vdegenne@gmail.com>
2021-01-28 02:52:27 +0100

    metadatas dialog temp stuffing

--- src/hanja-metadatas-dialog.ts
+++ src/hanja-metadatas-dialog.ts
@@ -1,6 +1,7 @@
 import { Dialog } from "@material/mwc-dialog";
 import { customElement, html, property } from "lit-element";
 import { render } from "lit-html";
+import { unsafeHTML } from "lit-html/directives/unsafe-html";
 import { HanjaMetadatas } from "./types";
 import '@material/mwc-button';
 import {app} from './app';
@@ -14,8 +15,14 @@ export class HanjaMetadatasDialog extends Dialog {
     if (this.metadatas) {
       this.heading = app.revealed ? this.metadatas.s : 'metadatas';
       render(html`
+      <div style="margin: 7px 0 15px;">${this.metadatas.e}</div>
       ${this.metadatas.p.map((p, i) => {
-        return html`${i+1}. ${p.t}`;
+        return html`
+        <div style="margin:0 0 15px 0;">
+          <div>${i+1}. <b>${p.t}</b></div>
+          <div style="padding: 5px 10px 0;white-space: break-spaces;">${unsafeHTML(p.k)}</div>
+        </div>
+        `;
       })}
       <mwc-button slot="primaryAction" dialogAction="close">close</mwc-button>
     `, this);
